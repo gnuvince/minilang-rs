0e05c1f96d8e43bec50e0d8126b65a0fe75a6cc5
Vincent Foley <vfoley@gmail.com>
2016-02-13 23:47:25 -0500

    Add clap support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 mod error;
 mod pos;
 mod token;
@@ -15,102 +18,106 @@ use parser::Parser;
 use typecheck::TypeChecker;
 
 use std::io::{Read, stdin};
-use std::env;
+use std::process;
 
-#[derive(PartialEq, Eq)]
-enum Command {
-    Scan,
-    Parse,
-    Typecheck,
-    Codegen,
-}
 
-fn compile(cmd: Command) -> Result<(), Error> {
-    let mut data = String::new();
-    let _ = stdin().read_to_string(&mut data);
-    let data = data;
-    let mut scanner = Scanner::new(&data);
-
-    let mut tokens: Vec<Token> = Vec::new();
+fn scan(display_tokens: bool) {
+    let mut stdin = stdin();
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf);
+    let mut scanner = Scanner::new(&buf);
 
     loop {
-        let tok = try!(scanner.next_token());
-        let is_eof = tok.typ == TokenType::Eof;
-        tokens.push(tok);
-        if is_eof {
-            break;
+        match scanner.next_token() {
+            Err(err) => {
+                println!("{:?}", err);
+                process::exit(1)
+            }
+            Ok(Token { typ: TokenType::Eof, .. }) => {}
+            Ok(tok) => {
+                if display_tokens {
+                    println!("{:?}", tok);
+                }
+            }
         }
     }
+}
 
-    if cmd == Command::Scan {
-        for tok in tokens {
-            println!("{:?}", tok);
+fn parse(display_ast: bool) {
+    let mut stdin = stdin();
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf);
+    let mut scanner = Scanner::new(&buf);
+
+    let mut tokens = Vec::new();
+    loop {
+        match scanner.next_token() {
+            Ok(tok) => {
+                let is_eof = tok.typ == TokenType::Eof;
+                tokens.push(tok);
+                if is_eof {
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("{:?}", err);
+                process::exit(1);
+            }
         }
-        return Ok(());
     }
 
     let mut parser = Parser::new(tokens);
-    let program = try!(parser.parse_program());
 
-    if cmd == Command::Parse {
-        println!("{:?}", program);
-        return Ok(());
-    }
-
-    let mut typechecker = TypeChecker::new();
-    try!(typechecker.tc_program(&program));
-
-    if cmd == Command::Typecheck {
-        println!("SYMBOL TABLE");
-        println!("============");
-        for (id, ty) in &typechecker.symtable {
-            println!("{}: {:?}", id, ty);
+    match parser.parse_program() {
+        Ok(ast) => {
+            if display_ast {
+                println!("{:#?}", ast);
+            }
         }
-
-        println!("");
-
-        println!("EXPRESSION TABLE");
-        println!("================");
-        for (expr, ty) in &typechecker.expr_table {
-            println!("{:?}: {:?}", expr, ty);
+        Err(err) => {
+            println!("{:?}", err);
+            process::exit(1);
         }
-        return Ok(())
-    }
-
-    cgen::codegen(&program, &typechecker.symtable, &typechecker.expr_table);
-
-    Ok(())
-}
-
-fn usage() {
-    match env::current_exe() {
-        Ok(path) => { println!("Usage: {} <scan | parse | typecheck | codegen>", path.display()); }
-        Err(_) => { println!("Usage: minic <scan | parse | typecheck | codegen>"); }
     }
 }
 
 fn main() {
-    match env::args().nth(1) {
-        Some(cmd) => {
-            let res =
-                if cmd == "scan" {
-                    compile(Command::Scan)
-                } else if cmd == "parse" {
-                    compile(Command::Parse)
-                } else if cmd == "typecheck" {
-                    compile(Command::Typecheck)
-                } else if cmd == "codegen" {
-                    compile(Command::Codegen)
-                } else {
-                    Err(Error::UsageError)
-                };
-            match res {
-                Ok(()) => (),
-                Err(Error::UsageError) => usage(),
-                Err(e) => { println!("Error: {}", e); }
-            }
+    let compiler_match = App::new("Minilang compiler")
+        .version("0.1")
+        .author("Vincent Foley <vfoley@gmail.com>")
+        .subcommand(SubCommand::with_name("scan")
+                    .about("Scan a program; return 0 if valid, 1 otherwise"))
 
+        .subcommand(SubCommand::with_name("tokens")
+                    .about("Scan a program and print its tokens one per line"))
+
+        .subcommand(SubCommand::with_name("parse")
+                    .about("Parse a program; return 0 if valid, 1 otherwise"))
+
+        .subcommand(SubCommand::with_name("ast")
+                    .about("Parse a program and print its AST"))
+
+        .subcommand(SubCommand::with_name("typecheck")
+                    .about("Typecheck a program; return 0 if valid, 1 otherwise"))
+
+        .subcommand(SubCommand::with_name("typed-ast")
+                    .about("Typecheck a program and print its typed AST"))
+
+        .subcommand(SubCommand::with_name("mips")
+                    .about("Generate MIPS code for a program"))
+
+
+
+        .get_matches();
+
+    match compiler_match.subcommand_name() {
+        Some("scan") => { scan(false) }
+        Some("tokens") => { scan(true) }
+        Some("parse") => { parse(false) }
+        Some("ast") => { parse(true) }
+        Some(_) => {}
+        None => {
+            println!("{}", compiler_match.usage());
         }
-        None => usage()
     }
 }
