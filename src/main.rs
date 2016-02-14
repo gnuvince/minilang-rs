a0be7ef60afe1f970821ef51f687237bcf0be317
Vincent Foley <vfoley@gmail.com>
2016-02-14 12:09:03 -0500

    Add CompileManager to clean up the code somewhat

--- src/main.rs
+++ src/main.rs
@@ -8,78 +8,85 @@ mod scanner;
 mod types;
 mod ast;
 mod parser;
-mod typecheck;
-mod cgen;
+// mod typecheck;
+// mod cgen;
 
 use token::{Token, TokenType};
 use error::Error;
 use scanner::Scanner;
 use parser::Parser;
-use typecheck::TypeChecker;
+// use typecheck::TypeChecker;
 
 use std::io::{Read, stdin};
 use std::process;
 
 
-fn scan(display_tokens: bool) {
-    let mut stdin = stdin();
-    let mut buf = String::new();
-    stdin.read_to_string(&mut buf);
-    let mut scanner = Scanner::new(&buf);
+struct CompileManager;
 
-    loop {
-        match scanner.next_token() {
-            Err(err) => {
-                println!("{:?}", err);
-                process::exit(1)
-            }
-            Ok(Token { typ: TokenType::Eof, .. }) => {}
-            Ok(tok) => {
-                if display_tokens {
-                    println!("{:?}", tok);
-                }
+enum CompileAction {
+    Scan,
+    DisplayTokens,
+    Parse,
+    DisplayAst,
+}
+
+impl CompileManager {
+    fn error(&self, err: Error) -> ! {
+        println!("{}", err);
+        process::exit(1);
+    }
+
+    fn perform_action(&self, action: CompileAction) {
+        match action {
+            CompileAction::Scan => { self.scan(false).unwrap_or_else(|e| self.error(e)) }
+            CompileAction::DisplayTokens => { self.scan(true).unwrap_or_else(|e| self.error(e)) }
+            CompileAction::Parse => { self.parse(false).unwrap_or_else(|e| self.error(e)) }
+            CompileAction::DisplayAst => { self.parse(true).unwrap_or_else(|e| self.error(e)) }
+        }
+    }
+
+    fn get_tokens(&self) -> Result<Vec<Token>, Error> {
+        let mut stdin = stdin();
+        let mut buf = String::new();
+        stdin.read_to_string(&mut buf);
+        let mut scanner = Scanner::new(&buf);
+
+        let mut tokens = Vec::new();
+        loop {
+            let tok = try!(scanner.next_token());
+            let is_eof = tok.typ == TokenType::Eof;
+            tokens.push(tok);
+            if is_eof {
+                break;
             }
         }
+        Ok(tokens)
+    }
+
+    // TODO(vfoley): don't build token vector if `display_tokens == false`.
+    fn scan(&self, display_tokens: bool) -> Result<(), Error> {
+        let tokens = try!(self.get_tokens());
+        if display_tokens {
+            for tok in tokens.iter() {
+                println!("{:?}", tok);
+            }
+        }
+        Ok(())
+    }
+
+
+    fn parse(&self, display_ast: bool) -> Result<(), Error> {
+        let tokens = try!(self.get_tokens());
+        let mut parser = Parser::new(tokens);
+        let ast = try!(parser.parse_program());
+        if display_ast {
+            println!("{:#?}", ast);
+        }
+        Ok(())
     }
 }
 
-fn parse(display_ast: bool) {
-    let mut stdin = stdin();
-    let mut buf = String::new();
-    stdin.read_to_string(&mut buf);
-    let mut scanner = Scanner::new(&buf);
 
-    let mut tokens = Vec::new();
-    loop {
-        match scanner.next_token() {
-            Ok(tok) => {
-                let is_eof = tok.typ == TokenType::Eof;
-                tokens.push(tok);
-                if is_eof {
-                    break;
-                }
-            }
-            Err(err) => {
-                println!("{:?}", err);
-                process::exit(1);
-            }
-        }
-    }
-
-    let mut parser = Parser::new(tokens);
-
-    match parser.parse_program() {
-        Ok(ast) => {
-            if display_ast {
-                println!("{:#?}", ast);
-            }
-        }
-        Err(err) => {
-            println!("{:?}", err);
-            process::exit(1);
-        }
-    }
-}
 
 fn main() {
     let compiler_match = App::new("Minilang compiler")
@@ -110,11 +117,12 @@ fn main() {
 
         .get_matches();
 
+    let cm = CompileManager;
     match compiler_match.subcommand_name() {
-        Some("scan") => { scan(false) }
-        Some("tokens") => { scan(true) }
-        Some("parse") => { parse(false) }
-        Some("ast") => { parse(true) }
+        Some("scan") => { cm.perform_action(CompileAction::Scan) }
+        Some("tokens") => { cm.perform_action(CompileAction::DisplayTokens) }
+        Some("parse") => { cm.perform_action(CompileAction::Parse) }
+        Some("ast") => { cm.perform_action(CompileAction::DisplayAst) }
         Some(_) => {}
         None => {
             println!("{}", compiler_match.usage());
